6190f71ec2b404ca815411fe830fe59ec402f5aa
Jacob V Sanoj <jacob@acciorobotics.com>
2024-05-22 10:11:01 +0530

    fix! : fixing issue at switch between -pi and pi by fixing angle calculation

--- diff_drive/include/diff_drive/EKFNode.h
+++ diff_drive/include/diff_drive/EKFNode.h
@@ -24,75 +24,70 @@
 /**
  * @brief Class for EKF based State Estimation
  * Estimates (x,y,yaw) based on odometry (v and omega) based predictions and IMU Accelerometer and Gyroscope yaw measurements
- * 
+ *
  */
-class EKFNode: public rclcpp::Node
+class EKFNode : public rclcpp::Node
 {
-    public:
-        /**
-         * @brief Construct a new EKFNode object
-         * 
-         * @param odom_topic_name    Topic to be subscribed to in order to read odometry information
-         * @param imu_topic_name     Topic to be subscribed to in order to read IMU measurement data
-         */
-        EKFNode(std::string odom_topic_name, std::string imu_topic_name): Node("EKF_node"){
-            // Reentrant callback group ensures callbacks work paralelly using multi-threading
-            odom_cb_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
-            imu_cb_group_= this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
-            rclcpp::SubscriptionOptions odom_sub_options;
-            odom_sub_options.callback_group = odom_cb_group_;
-            rclcpp::SubscriptionOptions imu_sub_options;
-            odom_sub_options.callback_group = imu_cb_group_;
-            odom_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
-      odom_topic_name, rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(), std::bind(&EKFNode::odom_callback, this, std::placeholders::_1), odom_sub_options);
-            imu_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
-      imu_topic_name, rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(), std::bind(&EKFNode::imu_callback, this, std::placeholders::_1), imu_sub_options);
-            ekf_publisher_ = this->create_publisher<geometry_msgs::msg::Pose>("/ekf_state", 10);
+public:
+    /**
+     * @brief Construct a new EKFNode object
+     *
+     * @param odom_topic_name    Topic to be subscribed to in order to read odometry information
+     * @param imu_topic_name     Topic to be subscribed to in order to read IMU measurement data
+     */
+    EKFNode(std::string odom_topic_name, std::string imu_topic_name) : Node("EKF_node")
+    {
+        // Reentrant callback group ensures callbacks work paralelly using multi-threading
+        odom_cb_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+        imu_cb_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+        rclcpp::SubscriptionOptions odom_sub_options;
+        odom_sub_options.callback_group = odom_cb_group_;
+        rclcpp::SubscriptionOptions imu_sub_options;
+        odom_sub_options.callback_group = imu_cb_group_;
+        odom_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            odom_topic_name, rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(), std::bind(&EKFNode::odom_callback, this, std::placeholders::_1), odom_sub_options);
+        imu_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
+            imu_topic_name, rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(), std::bind(&EKFNode::imu_callback, this, std::placeholders::_1), imu_sub_options);
+        ekf_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("/ekf_state", 10);
+    }
 
-        }
-    private:
-        /**
-         * @brief Callback for Odometry Subscriber (Performs Prediction Update)
-         * 
-         * @param msg const nav_msgs::msg::Odometry::SharedPtr
-         */
-        void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
-        /**
-         * @brief Callback for IMU Subscriber (Performs Measurement Update and Publishes EKF Estimated Pose)
-         * 
-         * @param msg const sensor_msgs::msg::Imu::SharedPtr
-         */
-        void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
-        /**
-         * @brief Helper method to get the Yaw from Quaternion object
-         * 
-         * @param quaternion 
-         * @return double returns yaw
-         */
-        double getYawfromQuaternion(const geometry_msgs::msg::Quaternion& quaternion);
-        // Shared pointers to callback groups, subscriber and publisher
-        rclcpp::CallbackGroup::SharedPtr odom_cb_group_;
-        rclcpp::CallbackGroup::SharedPtr imu_cb_group_;
-        rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr ekf_publisher_;
-        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscription_;
-        rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscription_;
-        // State
-        Eigen::Vector3d state_ = Eigen::Vector3d::Zero();
-        // State Covariance Matrix
-        Eigen::Matrix3d P_ {{10,0,0},{0,10,0},{0,0,10}};
-        
-        // Measurement Noise Covariance Matrix
-        double R_ = 0.01;
-        // Process Noise Covariance Matrix
-        Eigen::Matrix3d Q_ {{1,0,0},{0,1,0},{0,0,1}};
-        //Initial time
-        rclcpp::Time prev = this->get_clock()->now();  
-        
+private:
+    /**
+     * @brief Callback for Odometry Subscriber (Performs Prediction Update)
+     *
+     * @param msg const nav_msgs::msg::Odometry::SharedPtr
+     */
+    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    /**
+     * @brief Callback for IMU Subscriber (Performs Measurement Update and Publishes EKF Estimated Pose)
+     *
+     * @param msg const sensor_msgs::msg::Imu::SharedPtr
+     */
+    void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
+    /**
+     * @brief Helper method to get the Yaw from Quaternion object
+     *
+     * @param quaternion
+     * @return double returns yaw
+     */
+    double getYawfromQuaternion(const geometry_msgs::msg::Quaternion &quaternion);
+    // Shared pointers to callback groups, subscriber and publisher
+    rclcpp::CallbackGroup::SharedPtr odom_cb_group_;
+    rclcpp::CallbackGroup::SharedPtr imu_cb_group_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr ekf_publisher_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscription_;
+    // State
+    Eigen::Vector3d state_ = Eigen::Vector3d::Zero();
+    // State Covariance Matrix
+    Eigen::Matrix3d P_{{10, 0, 0}, {0, 10, 0}, {0, 0, 10}};
 
-
+    // Measurement Noise Covariance Matrix
+    double R_ = 0.01;
+    // Process Noise Covariance Matrix
+    Eigen::Matrix3d Q_{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+    // Initial time
+    rclcpp::Time prev = this->get_clock()->now();
 };
-
-
-
 
 #endif
--- diff_drive/src/EKFNode.cpp
+++ diff_drive/src/EKFNode.cpp
@@ -1,7 +1,7 @@
 #include "diff_drive/EKFNode.h"
 
-
-double EKFNode::getYawfromQuaternion(const geometry_msgs::msg::Quaternion& quaternion){
+double EKFNode::getYawfromQuaternion(const geometry_msgs::msg::Quaternion &quaternion)
+{
     // Convert the quaternion to Euler angles (roll, pitch, yaw)
     tf2::Quaternion tf_quat;
     tf2::convert(quaternion, tf_quat);
@@ -11,7 +11,8 @@ double EKFNode::getYawfromQuaternion(const geometry_msgs::msg::Quaternion& quate
     return yaw;
 }
 
-void EKFNode::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg){
+void EKFNode::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
     RCLCPP_INFO(this->get_logger(), "Initial State: [x = '%f', y = '%f', yaw= '%f']", state_[0], state_[1], state_[2]);
     RCLCPP_INFO(this->get_logger(), "Actual State x and y: [x = '%f', y = '%f']", msg->pose.pose.position.x, msg->pose.pose.position.y);
     RCLCPP_INFO(this->get_logger(), "Odometry=[v = '%f', omega = '%f']", msg->twist.twist.linear.x, msg->twist.twist.angular.z);
@@ -20,35 +21,43 @@ void EKFNode::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg){
     double v = msg->twist.twist.linear.x;
     double omega = msg->twist.twist.angular.z;
     double delta = now.seconds() - prev.seconds();
-    std::array<double,36> vel_cov = msg->twist.covariance;
+    std::array<double, 36> vel_cov = msg->twist.covariance;
 
     Eigen::Matrix<double, 6, 6> cov_odom;
 
     cov_odom << vel_cov[0], vel_cov[1], vel_cov[2], vel_cov[3], vel_cov[4], vel_cov[5],
-                vel_cov[6], vel_cov[7], vel_cov[8], vel_cov[9], vel_cov[10], vel_cov[11],
-                vel_cov[12], vel_cov[13], vel_cov[14], vel_cov[15], vel_cov[16], vel_cov[17],
-                vel_cov[18], vel_cov[19], vel_cov[20], vel_cov[21], vel_cov[22], vel_cov[23],
-                vel_cov[24], vel_cov[25], vel_cov[26], vel_cov[27], vel_cov[28], vel_cov[29],
-                vel_cov[30], vel_cov[31], vel_cov[32], vel_cov[33], vel_cov[34], vel_cov[35];
-    
-    Q_(0,0) = cov_odom(0,0);
-    Q_(1,1) = cov_odom(1,1);
-    Q_(2,2) = cov_odom(5,5);
+        vel_cov[6], vel_cov[7], vel_cov[8], vel_cov[9], vel_cov[10], vel_cov[11],
+        vel_cov[12], vel_cov[13], vel_cov[14], vel_cov[15], vel_cov[16], vel_cov[17],
+        vel_cov[18], vel_cov[19], vel_cov[20], vel_cov[21], vel_cov[22], vel_cov[23],
+        vel_cov[24], vel_cov[25], vel_cov[26], vel_cov[27], vel_cov[28], vel_cov[29],
+        vel_cov[30], vel_cov[31], vel_cov[32], vel_cov[33], vel_cov[34], vel_cov[35];
 
-    Eigen::Vector3d increment(v*delta*cos(state_[2] + omega*delta), v*delta*sin(state_[2] + omega*delta), omega*delta);
+    Q_(0, 0) = cov_odom(0, 0);
+    Q_(1, 1) = cov_odom(1, 1);
+    Q_(2, 2) = cov_odom(5, 5);
+
+    Eigen::Vector3d increment(v * delta * cos(state_[2] + omega * delta), v * delta * sin(state_[2] + omega * delta), omega * delta);
     Eigen::Matrix3d Gk;
-    std::cout << "Time step: " << delta << ", Theta new: " << (state_[2] + omega*delta) << std::endl;
-    Gk << 1,0,-1.0*sin(state_[2] + omega*delta), 0,1,cos(state_[2] + omega*delta), 0,0,1;
+    std::cout << "Time step: " << delta << ", Theta new: " << (state_[2] + omega * delta) << std::endl;
+    Gk << 1, 0, -1.0 * sin(state_[2] + omega * delta), 0, 1, cos(state_[2] + omega * delta), 0, 0, 1;
     std::cout << "Gk: " << Gk << std::endl;
     // Predict state based on Odometry
     state_ = state_ + increment;
     // Predict state covariance matrix
-    P_ = (Gk*P_)*Gk.transpose() + Q_;
+    P_ = (Gk * P_) * Gk.transpose() + Q_;
     // Update prev
     prev = this->get_clock()->now();
-    
 }
-void EKFNode::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg){
+double normalizeAngle(double angle)
+{
+    while (angle > M_PI)
+        angle -= 2.0 * M_PI;
+    while (angle < -M_PI)
+        angle += 2.0 * M_PI;
+    return angle;
+}
+void EKFNode::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+{
     RCLCPP_INFO(this->get_logger(), "Initial State: [x = '%f', y = '%f', yaw= '%f']", state_[0], state_[1], state_[2]);
     std::array<double, 9> orientation_covariance = msg->orientation_covariance;
     double epsilon = 1e-3;
@@ -60,25 +69,31 @@ void EKFNode::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg){
     Ck[2] = 1.0;
     // Implement Measurement Update Step
     // Calculate Kalman Gain (Note that Ck*P_*Ck + R_ is a double since only 1 measurement)
-    Eigen::Vector3d K = (P_*Ck.transpose())*(1.0/(Ck.dot(P_*Ck.transpose()) + R_));
-    if(P_*Ck.transpose() == Eigen::Vector3d::Zero()){
+    Eigen::Vector3d K = (P_ * Ck.transpose()) * (1.0 / (Ck.dot(P_ * Ck.transpose()) + R_));
+    if (P_ * Ck.transpose() == Eigen::Vector3d::Zero())
+    {
         K = Eigen::Vector3d::Zero();
     }
     std::cout << "K: " << K << std::endl;
+    double yaw_diff = normalizeAngle(yaw_measured - Ck.dot(state_));
+
     // Correct State Prediction
-    state_ = state_ + K*(yaw_measured - Ck.dot(state_));
+    state_ = state_ + K * yaw_diff;
     // Correct State Covariance Matrix Prediction
-    P_ = P_ - K*(Ck*P_); 
+    P_ = P_ - K * (Ck * P_);
     // Publish States on the /ekf_state topic
-    auto msg1 = std::make_shared<geometry_msgs::msg::Pose>();
-    msg1->position.x = state_[0];
-    msg1->position.y = state_[1];
+    auto msg1 = std::make_shared<nav_msgs::msg::Odometry>();
+    msg1->header.stamp = this->get_clock()->now();
+    msg1->header.frame_id = "odom";
+    msg1->child_frame_id = "base_link";
+    msg1->pose.pose.position.x = state_[0];
+    msg1->pose.pose.position.y = state_[1];
     tf2::Quaternion q;
-    q.setRPY(0.0,0.0,state_[2]);
-    msg1->orientation.x = q.x();
-    msg1->orientation.y = q.y();
-    msg1->orientation.z = q.z();
-    msg1->orientation.w = q.w();
-    RCLCPP_INFO(this->get_logger(),"Estimated State: [x = '%f', y = '%f', yaw = '%f']", state_[0], state_[1], state_[2]);
+    q.setRPY(0.0, 0.0, state_[2]);
+    msg1->pose.pose.orientation.x = q.x();
+    msg1->pose.pose.orientation.y = q.y();
+    msg1->pose.pose.orientation.z = q.z();
+    msg1->pose.pose.orientation.w = q.w();
+    RCLCPP_INFO(this->get_logger(), "Estimated State: [x = '%f', y = '%f', yaw = '%f']", state_[0], state_[1], state_[2]);
     ekf_publisher_->publish(*msg1);
 }
